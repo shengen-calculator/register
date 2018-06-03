ec1bc35dc3fd6e47a67abb74691261e223ae008f
Suddya-Vasyl <vasya_mail@ukr.net>
2018-06-03 07:39:58 +0300

    add days counter handlers

--- src/reducers/tripsReducer.js
+++ src/reducers/tripsReducer.js
@@ -1,6 +1,5 @@
 import initialState from './initialState';
 import * as types from '../actions/actionTypes';
-import moment from 'moment';
 
 
 export default function tripsReducer(state = initialState.trips, action) {
@@ -12,13 +11,12 @@ export default function tripsReducer(state = initialState.trips, action) {
                 if (oldTrip.length === 0) {
                     return [
                         ...state,
-                        Object.assign({}, action.trip.back ? {
+                        Object.assign({}, {
                             id: action.trip.id,
                             out: action.trip.out,
-                            back: action.trip.back
-                        } : {
-                            id: action.trip.id,
-                            out: action.trip.out
+                            back: action.trip.back,
+                            days: action.trip.days,
+                            counted: action.trip.counted
                         })
                     ];
 
@@ -34,32 +32,28 @@ export default function tripsReducer(state = initialState.trips, action) {
             if (action.trip) {
                 return [
                     ...state.filter(trip => trip.id !== action.trip.id),
-                    Object.assign({}, action.trip)
+                    Object.assign({}, {
+                        id: action.trip.id,
+                        out: action.trip.out,
+                        back: action.trip.back,
+                        days: action.trip.days,
+                        counted: action.trip.counted
+                    })
                 ];
             } else {
                 return state;
             }
 
         case types.LOAD_TRIPS_SUCCESS:
-            let outMoment;
-            let backMoment;
+
             if(action.trips) {
-                return Object.keys(action.trips).map((el) => {
-
-                    outMoment = moment.unix(action.trips[el].out);
-                    const correction = outMoment.diff(backMoment, 'days') === 0 ? 1 : 0;
-
-                    backMoment = moment.unix(action.trips[el].back);
-
-                    const days = backMoment.diff(outMoment, 'days') === 0 ? 1 :
-                        backMoment.diff(outMoment, 'days');
-
+                return action.trips.map((el) => {
                     return {
-                        id: el,
-                        out: action.trips[el].out,
-                        back: action.trips[el].back,
-                        days: days,
-                        counted: days - correction
+                        id: el.id,
+                        out: el.out,
+                        back: el.back,
+                        days: el.days,
+                        counted: el.counted
                     };
                 });
             } else {
--- src/services/dataService.js
+++ src/services/dataService.js
@@ -3,6 +3,32 @@ import {ajaxCallError, beginAjaxCall} from "../actions/ajaxStatusActions";
 import {
     addTripSuccess, updateTripSuccess, tripOutSuccess, tripBackSuccess, loadTripsSuccess
 } from "../actions/tripActions";
+import moment from 'moment';
+
+
+function tripHandle(trip, id, backMoment) {
+
+    let days = 0;
+
+    let outMoment =moment.unix(trip.out);
+    let correction = outMoment.diff(backMoment, 'days') === 0 ? 1 : 0;
+
+    if(trip.back) {
+        backMoment = moment.unix(trip.back);
+        days = backMoment.diff(outMoment, 'days') + 1;
+    } else {
+        const todayDiff = moment().diff(outMoment, 'days');
+        days = todayDiff >= 0 ? todayDiff + 1 : 1;
+    }
+
+    return {
+        id: id,
+        out: trip.out,
+        back: trip.back,
+        days: days,
+        counted: days - correction
+    };
+}
 
 
 export function out(uid, dateTime) {
@@ -21,7 +47,7 @@ export function back(uid, dateTime, tripId) {
     return function (dispatch, getState) {
         dispatch(beginAjaxCall());
         return tripApi.back(uid, dateTime, tripId).then(() => {
-            dispatch(tripBackSuccess())
+            dispatch(tripBackSuccess());
         }).catch(error => {
             dispatch(ajaxCallError(error));
             throw(error);
@@ -30,18 +56,28 @@ export function back(uid, dateTime, tripId) {
     };
 }
 
+
 export function getTrips(uid) {
     return function (dispatch, getState) {
         dispatch(beginAjaxCall());
-        return tripApi.loadTrips(uid).then((trips) => {
-            dispatch(loadTripsSuccess(trips.val()));
 
+        return tripApi.loadTrips(uid).then((x) => {
+            let backMoment;
+            const trips = x.val();
+            if(trips) {
+                const result = Object.keys(trips).map((el) => {
+                    const prevMoment = backMoment;
+                    backMoment = moment.unix(trips[el].back);
+                    return tripHandle(trips[el], el, prevMoment);
+                });
+                dispatch(loadTripsSuccess(result));
+            }
+            dispatch(loadTripsSuccess());
 
         }).catch(error => {
             dispatch(ajaxCallError(error));
             throw(error);
         });
-
     };
 }
 
@@ -56,12 +92,20 @@ export function startListenDataChanges(uid, errorHandler) {
         tripApi.subscribeTripsAdded(uid, function (snapshot) {
             dispatch(beginAjaxCall());
             const trip = snapshot.val();
-            dispatch(addTripSuccess({
-                    id: snapshot.key,
-                    out: trip.out,
-                    back: trip.back
-                }
-            ));
+
+            let prevBackMoment;
+
+            if(getState().trips.length > 0) {
+                prevBackMoment = moment.unix(getState().trips.slice(-1)[0].back);
+            }
+
+            const handledTrip = tripHandle(
+                {
+                    out: trip.out
+                }, snapshot.key, prevBackMoment);
+
+            dispatch(addTripSuccess(handledTrip));
+
         }, function (error) {
             dispatch(ajaxCallError(error));
             errorHandler(error);
@@ -70,12 +114,20 @@ export function startListenDataChanges(uid, errorHandler) {
         tripApi.subscribeTripsChanged(uid, function (snapshot) {
             dispatch(beginAjaxCall());
             const trip = snapshot.val();
-            dispatch(updateTripSuccess({
-                    id: snapshot.key,
+            let prevBackMoment;
+
+            if(getState().trips.length > 1) {
+                prevBackMoment = moment.unix(getState().trips.slice(-2)[0].back);
+            }
+
+            const handledTrip = tripHandle(
+                {
                     out: trip.out,
                     back: trip.back
-                }
-            ));
+                }, snapshot.key, prevBackMoment);
+
+            dispatch(updateTripSuccess(handledTrip));
+
         }, function (error) {
             dispatch(ajaxCallError(error));
             errorHandler(error);
