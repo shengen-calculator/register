29c8f49b96aa15fbdf523b9acd893f3c13d71a12
v-suddya <suddya@toptal.com>
2018-12-25 09:15:55 +0200

    update text for help page

--- src/components/help/HelpPage.js
+++ src/components/help/HelpPage.js
@@ -2,6 +2,24 @@ import React from 'react';
 import './Help.css';
 
 const HelpPage = () => {
+    const startDate = new Date();
+    startDate.setDate(startDate.getDate() - 180);
+
+    function formatDate(date) {
+        const monthNames = [
+            "січня", "лютого", "березня",
+            "квітня", "травня", "червня", "липня",
+            "серпня", "вересня", "жовтня",
+            "листопада", "грудня"
+        ];
+
+        const day = date.getDate();
+        const monthIndex = date.getMonth();
+        const year = date.getFullYear();
+
+        return `${day} ${monthNames[monthIndex]} ${year}p.`;
+    }
+
     return (
         <div className='help-page'>
             <h1>Довідка</h1>
@@ -9,16 +27,20 @@ const HelpPage = () => {
                 Червоне число зліва вказує на кількість днів перебування в країнах Шенгенської
                 угоди за останні 180 днів (при подорожах за безвізом це число не повинно перевищувати
                 90). Зелене число справа вказує на кількістів днів, що минуло з часу повернення після
-                останньої подорожі. Ця інформація може бути цікава для "пересічників" або ж для людей
-                що заїхали на територію України з європейським паспортом, щоб не перевищити дозволений
-                час перебування.
+                останньої подорожі. Ця інформація може бути цікава для людей що заїхали на територію
+                України з європейським паспортом, щоб не перевищити дозволений час перебування.
+            </p>
+            <p className="text">
+                Якщо Ви тільки почали користуватись сервісом, не забудьте перед початком роботи внести
+                інформацію про всі Ваші минулі подорожі за останні 180 днів. Тобто починаючи з &nbsp;
+                <b>{formatDate(startDate)}</b>
             </p>
             <br/>
             <p className="text">
                 В випадку будь-яких незручностей Ви можете контактувати з нами за адресою
             </p>
             <p>
-                <a href="mailto:info@shengen.eu?subject=Users%20support">info@shengen.eu</a>
+                <a href="mailto:info@shengen.me?subject=Users%20support">info@shengen.me</a>
             </p>
             <p>&nbsp;</p>
         </div>
